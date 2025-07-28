001be449bdf08a2ccf9908e18b04ecde1dc71834
Eric Pedley <ericpedley@gmail.com>
2025-07-28 15:39:03 -0700

    get arms to spawn and twitch randomly

--- isaac_lab_main.py
+++ isaac_lab_main.py
@@ -24,10 +24,184 @@ args_cli = parser.parse_args()
 app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
-"""Rest everything follows."""
-
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets import AssetBaseCfg, Articulation
+from isaaclab.assets.articulation import ArticulationCfg
 from isaaclab.sim import SimulationCfg, SimulationContext
+import isaacsim.core.utils.prims as prim_utils
+from isaaclab.sim.utils import attach_stage_to_usd_context
 
+import isaaclab.sim as sim_utils
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+import isaacsim.core.utils.stage as stage_utils
+import torch
+
+from pathlib import Path
+
+
+so101_usd_path = Path(__file__).parent / 'models/SO101/so101_new_calib/so101_new_calib.usd'
+
+SO101_CONFIG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=str(so101_usd_path),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=True,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "shoulder_pan": 0.0,
+            "shoulder_lift": 0.0,
+            "elbow_flex": 0.0,
+            "wrist_flex": 0.0,
+            "wrist_roll": 0.0,
+            "gripper": 0.0,
+        },
+        pos=(0.0, 0.0, 0.0),
+    ),
+    actuators={
+        "shoulder_pan": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_pan"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),
+        "shoulder_lift": ImplicitActuatorCfg(
+            joint_names_expr=["shoulder_lift"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),
+        "elbow_flex": ImplicitActuatorCfg(
+            joint_names_expr=["elbow_flex"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),  
+        "wrist_flex": ImplicitActuatorCfg(
+            joint_names_expr=["wrist_flex"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),  
+        "wrist_roll": ImplicitActuatorCfg(
+            joint_names_expr=["wrist_roll"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),  
+        "gripper": ImplicitActuatorCfg(
+            joint_names_expr=["gripper"],
+            effort_limit_sim=100.0,
+            velocity_limit_sim=100.0,
+            stiffness=10000.0,
+            damping=100.0,
+        ),  
+        
+    },
+)
+
+def design_scene():
+    """Designs the scene by spawning ground plane, light, objects and meshes from usd files."""
+    # Ground-plane
+    cfg_ground = sim_utils.GroundPlaneCfg()
+    cfg_ground.func("/World/defaultGroundPlane", cfg_ground)
+
+    # spawn distant light
+    cfg_light_distant = sim_utils.DistantLightCfg(
+        intensity=3000.0,
+        color=(0.75, 0.75, 0.75),
+    )
+    cfg_light_distant.func("/World/lightDistant", cfg_light_distant, translation=(1, 0, 10))
+
+    # create a new xform prim for all objects to be spawned under
+    prim_utils.create_prim("/World/Objects", "Xform")
+
+    # spawn a blue cuboid with deformable body
+    blue_cube_cfg = sim_utils.MeshCuboidCfg(
+        size=(0.2, 0.2, 0.2),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 0.0, 1.0)),
+    )
+    blue_cube_cfg.func("/World/Objects/BlueCube", blue_cube_cfg, translation=(0.15, 0.2, 2.0))
+
+    green_cube_cfg = sim_utils.MeshCuboidCfg(
+        size=(0.2, 0.2, 0.2),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+        visual_material=sim_utils.PreviewSurfaceCfg(diffuse_color=(0.0, 1.0, 0.0)),
+    )
+    green_cube_cfg.func("/World/Objects/GreenCube", green_cube_cfg, translation=(0.45, 0.0, 2.0))
+
+    # spawn a usd file of a table into the scene
+    # cfg = sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd")
+    # cfg.func("/World/Objects/Table", cfg, translation=(0.0, 0.0, 1.05))
+
+    origins = [[0.0, 0.0, 0.0], [-1.0, 0.0, 0.0]]
+    # Origin 1
+    prim_utils.create_prim("/World/Origin1", "Xform", translation=origins[0])
+    # Origin 2
+    prim_utils.create_prim("/World/Origin2", "Xform", translation=origins[1])
+
+    so101_config = SO101_CONFIG.copy()
+    so101_config.prim_path = "/World/Origin.*/Robot"
+    # cartpole_cfg = CARTPOLE_CFG.copy()
+    # cartpole_cfg.prim_path = "/World/Origin.*/Robot"
+    so101 = Articulation(cfg=so101_config)
+
+    scene_entities = {"so101": so101}
+    return scene_entities, origins
+
+def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articulation], origins: torch.Tensor):
+    """Runs the simulation loop."""
+    # Extract scene entities
+    # note: we only do this here for readability. In general, it is better to access the entities directly from
+    #   the dictionary. This dictionary is replaced by the InteractiveScene class in the next tutorial.
+    robot = entities["so101"]
+    # Define simulation stepping
+    sim_dt = sim.get_physics_dt()
+    count = 0
+    # Simulation loop
+    while simulation_app.is_running():
+        # Reset
+        if count % 500 == 0:
+            # reset counter
+            count = 0
+            # reset the scene entities
+            # root state
+            # we offset the root state by the origin since the states are written in simulation world frame
+            # if this is not done, then the robots will be spawned at the (0, 0, 0) of the simulation world
+            root_state = robot.data.default_root_state.clone()
+            root_state[:, :3] += origins
+            robot.write_root_pose_to_sim(root_state[:, :7])
+            robot.write_root_velocity_to_sim(root_state[:, 7:])
+            # set joint positions with some noise
+            joint_pos, joint_vel = robot.data.default_joint_pos.clone(), robot.data.default_joint_vel.clone()
+            joint_pos += torch.rand_like(joint_pos) * 0.1
+            robot.write_joint_state_to_sim(joint_pos, joint_vel)
+            # clear internal buffers
+            robot.reset()
+            print("[INFO]: Resetting robot state...")
+        # Apply random action
+        # -- generate random joint efforts
+        efforts = torch.randn_like(robot.data.joint_pos) * 5.0
+        # -- apply action to the robot
+        robot.set_joint_effort_target(efforts)
+        # -- write data to sim
+        robot.write_data_to_sim()
+        # Perform step
+        sim.step()
+        # Increment counter
+        count += 1
+        # Update buffers
+        robot.update(sim_dt)
 
 def main():
     """Main function."""
@@ -38,15 +212,18 @@ def main():
     # Set main camera
     sim.set_camera_view([2.5, 2.5, 2.5], [0.0, 0.0, 0.0])
 
+    with stage_utils.use_stage(sim.get_initial_stage()):
+        scene_entities, scene_origins = design_scene()
+        attach_stage_to_usd_context()
+    scene_origins = torch.tensor(scene_origins, device=sim.device)
+
     # Play the simulator
     sim.reset()
     # Now we are ready!
     print("[INFO]: Setup complete...")
 
     # Simulate physics
-    while simulation_app.is_running():
-        # perform step
-        sim.step()
+    run_simulator(sim, scene_entities, scene_origins)
 
 
 if __name__ == "__main__":
